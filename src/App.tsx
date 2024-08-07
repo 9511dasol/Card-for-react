4a8d37188f04713f1c7701987dacc6e55238b1f4
DaSolHan <9511dasole@gmail.com>
2024-08-07 20:19:45 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,27 @@ import {
 import Homepage from "./components/Homepage";
 import Usermain from "./components/userPage/Usermain";
 import Findcard from "./components/userPage/Findcard";
+import Recommand from "./components/userPage/Recommand";
+import Faq from "./components/userPage/Faq";
+import Famouscard from "./components/userPage/Famouscard";
+import Contact from "./components/managerPage/Contact";
+import Mfaq from "./components/managerPage/Mfaq";
+import Main from "./components/managerPage/Main";
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Homepage />}>
+        {/* 유저 모드 */}
           <Route index element={<Usermain />} />
           <Route path="/findcard" element={<Findcard />} />
+          <Route path="/recommnad" element={<Recommand />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/famouscard" element={<Famouscard />} />
+          {/* 관리잠 모드 */}
+          <Route path="/recommnad" element={<Main />} />
+          <Route path="/recommnad" element={<Contact />} />
+          <Route path="/recommnad" element={<Mfaq />} />
         </Route>
       </Routes>
     </Router>
